48b5523fe835596f14dea17a4330ae13ec6e56e2
NaumanMunir9 <naumanmunir9@gmail.com>
2021-11-09 15:11:27 +0500

    lambda

--- lib/ex1-define-userpool-stack.ts
+++ lib/ex1-define-userpool-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "@aws-cdk/core";
 import * as cognito from "@aws-cdk/aws-cognito";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class Ex1DefineUserpoolStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,6 +8,14 @@ export class Ex1DefineUserpoolStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    // Lambda Function
+    const authEmailFn = new lambda.Function(this, "authEmailFn", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("lambda"),
+    });
+
+    // Cognito - UserPool
     const userPool = new cognito.UserPool(this, "UserPool", {
       selfSignUpEnabled: true, // Allow users to sign up
       autoVerify: { email: true }, // Verify email addresses by sending a verification code
@@ -18,6 +27,9 @@ export class Ex1DefineUserpoolStack extends cdk.Stack {
         emailStyle: cognito.VerificationEmailStyle.CODE,
         smsMessage:
           "Hello {username}, Thanks for signing up to our awesome app! Your verification code is {####}",
+      },
+      lambdaTriggers: {
+        preSignUp: authEmailFn, ///Trigger before the signup process to userpool
       },
     });
   }
